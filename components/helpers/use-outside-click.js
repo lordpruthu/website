39c1f1abb56cd194f929855a4a390e119672a289
Pruthu Raut <pruthu.raut@somaiya.edu>
2023-10-03 23:22:38 +0530

    fixed the block of text inside case studie

--- components/helpers/use-outside-click.js
+++ components/helpers/use-outside-click.js
@@ -1,21 +1,26 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect } from 'react';
 
 export function useOutsideClick(callback) {
   const callbackRef = useRef();
   const innerRef = useRef();
 
-  useEffect(() => { callbackRef.current = callback; });
-  
+  useEffect(() => {
+    callbackRef.current = callback;
+  });
+
   useEffect(() => {
     function handleClick(e) {
-      if (innerRef.current && callbackRef.current && 
+      if (
+        innerRef.current &&
+        callbackRef.current &&
         !innerRef.current.contains(e.target)
-      ) callbackRef.current(e);
+      )
+        callbackRef.current(e);
     }
 
-    document.addEventListener("click", handleClick);
-    return () => document.removeEventListener("click", handleClick);
+    document.addEventListener('click', handleClick);
+    return () => document.removeEventListener('click', handleClick);
   }, []);
-      
+
   return innerRef;
 }
